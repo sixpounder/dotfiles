767cb7316f961ca9d59ccbfd22c320249a488641
Andrea Coronese <sixpounder@protonmail.com>
2025-12-14 12:00:23 +0100

    feat: first customizations

--- fish/config.fish
+++ fish/config.fish
@@ -3,14 +3,14 @@ if status is-interactive
     starship init fish | source
 
     # Direnv + Zoxide
-    command -v direnv &> /dev/null && direnv hook fish | source
-    command -v zoxide &> /dev/null && zoxide init fish --cmd cd | source
+    command -v direnv &>/dev/null && direnv hook fish | source
+    command -v zoxide &>/dev/null && zoxide init fish --cmd cd | source
 
     # Better ls
-    alias ls='eza --icons --group-directories-first -1'
+    alias ls='eza -lh --icons --group-directories-first -1'
 
     # Abbrs
-    abbr lg 'lazygit'
+    abbr lg lazygit
     abbr gd 'git diff'
     abbr ga 'git add .'
     abbr gc 'git commit -am'
@@ -27,13 +27,13 @@ if status is-interactive
     abbr gco 'git checkout'
     abbr gsh 'git show'
 
-    abbr l 'ls'
+    abbr l ls
     abbr ll 'ls -l'
     abbr la 'ls -a'
     abbr lla 'ls -la'
 
     # Custom colours
-    cat ~/.local/state/caelestia/sequences.txt 2> /dev/null
+    cat ~/.local/state/caelestia/sequences.txt 2>/dev/null
 
     # For jumping between prompts in foot terminal
     function mark_prompt_start --on-event fish_prompt
